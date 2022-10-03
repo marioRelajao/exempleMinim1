52ff6c63fce7487e6710cee85282fcfa96ba5c1e
marioRelajao <mario.cerda@estudiantat.upc.edu>
2022-10-03 13:51:22 +0200

    Afegim dades clase User

--- src/main/java/edu/upc/eetac/dsa/models/User.java
+++ src/main/java/edu/upc/eetac/dsa/models/User.java
@@ -1,4 +1,21 @@
 package edu.upc.eetac.dsa.models;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class User {
+    private String userID;
+
+    private String name;
+
+    private String surname;
+
+    private List<Order> orders;     //Array de order
+
+    public User(String userID, String name, String surname) {       //Constructor del User
+        this.userID = userID;
+        this.name = name;
+        this.surname = surname;
+        this.orders = new LinkedList<>();       //Creamos una linked-list vacia
+    }
 }
