592f48d81bbd9008cc242c99d2c96e634e7877f2
Marcel Marco <marco.marcel@estudiantat.upc.edu>
2022-10-03 13:48:46 +0200

    Añadidos metodos de la classe Product

--- src/main/java/edu/upc/eetac/dsa/ProductManagerImpl.java
+++ src/main/java/edu/upc/eetac/dsa/ProductManagerImpl.java
@@ -1,4 +1,10 @@
 package edu.upc.eetac.dsa;
 
+import java.util.HashMap;
+import java.util.Queue;
+
 public class ProductManagerImpl {
+    List<Product> products;
+    HashMap<String, User> users;
+    Queue<Order> orders;
 }
--- src/main/java/edu/upc/eetac/dsa/models/Product.java
+++ src/main/java/edu/upc/eetac/dsa/models/Product.java
@@ -1,4 +1,21 @@
 package edu.upc.eetac.dsa.models;
 
 public class Product {
+    private String productId;
+    private String name;
+    private double price;
+    private double sales;
+
+    public String getProductId(){
+        return this.productId;
+    }
+    public double getPrice(){
+        return this.price;
+    }
+    public String getDescription(){
+        return this.name;
+    }
+    public double getNumSales(){
+        return this.sales;
+    }
 }
